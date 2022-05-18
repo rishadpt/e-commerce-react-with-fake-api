b07a420ed9056e2d53d3359bf734c7253f3e90d2
rishadpt <rishadpt16@gmail.com>
2022-05-18 23:56:26 +0530

    image link path fixed

--- src/Components/CategoryDisplay/MainContainer/MainContainer.js
+++ src/Components/CategoryDisplay/MainContainer/MainContainer.js
@@ -4,12 +4,12 @@ import './MainContainer.scss'
 export default function MainContainer() {
     return (
         <div className="main_card_container">
-            <div className="card_title_container">
+            <div className="card_title_container"> 
                 <h3 className="main_card_title">Women's Fashion</h3>
                 <small> Lorem Ipsum has been the industry's standard dummy <br /> text ever since the 1500s  make a type specimen book</small>
                 <p >SHOP NOW</p>
             </div>
-        <img src="/images/Cards/maincard.png" alt=""/>
+        <img src="/images/Cards/Maincard.png" alt="Maincardimage"/>
         </div>
     )
 }
